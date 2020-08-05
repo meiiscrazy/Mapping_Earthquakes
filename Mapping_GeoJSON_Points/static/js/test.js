6465860e5fbd99b0c91af72e1b9197df8c176a9c
Meilee Cheng <mei@Meilees-MacBook-Pro.local>
2020-08-05 15:12:14 -0700

    Adding Mapping GeoJSON

---
+++ Mapping_GeoJSON_Points/static/js/test.js
@@ -0,0 +1,36 @@
+// // Add GeoJSON data.
+// let sanFranAirport =
+// {"type":"FeatureCollection","features":[{
+//     "type":"Feature",
+//     "properties":{
+//         "id":"3469",
+//         "name":"San Francisco International Airport",
+//         "city":"San Francisco",
+//         "country":"United States",
+//         "faa":"SFO",
+//         "icao":"KSFO",
+//         "alt":"13",
+//         "tz-offset":"-8",
+//         "dst":"A",
+//         "tz":"America/Los_Angeles"},
+//         "geometry":{
+//             "type":"Point",
+//             "coordinates":[-122.375,37.61899948120117]}}
+// ]};
+
+// // Grabbing our GeoJSON data.
+// L.geoJson(sanFranAirport, {
+//   onEachFeature: function(feature, layer) {
+//     console.log(layer);
+//     layer.bindPopup("<h2> Airport Code: " + feature.properties.faa + "<h2> <hr> Airport name: " + feature.properties.name);
+//   }
+// }).addTo(map);
+// L.geoJson(sanFranAirport, {
+//   // We turn each feature into a marker on the map.
+//   pointToLayer: function(feature, latlng) {
+//     console.log(feature);
+//     return L.marker(latlng)
+//     .bindPopup("<h2>" + feature.properties.name + "</h2> <hr>" + feature.properties.city + ", " + feature.properties.country);
+//   }
+// }).addTo(map);
+// streets.addTo(map);
